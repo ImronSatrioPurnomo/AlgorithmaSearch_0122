8f421e12a426d32bcba295fc0a081a113ee9ece7
ImronSatrioPurnomo <satriogames2223@gmail.com>
2024-04-05 09:51:50 +0700

    menambahkan code untuk menerima elemen array

--- AlgoritmaSearch/AlgoritmaSearch.cpp
+++ AlgoritmaSearch/AlgoritmaSearch.cpp
@@ -9,5 +9,16 @@ void input() {
 	while (true) {
 		cout << "memasukan nomor elemen ke dalam array : ";
 		cin >> n;
+		if ((n > 0) && (n <= 20))
+			break;
+		else
+			cout << "\nArray harus memiliki minimal 1 dan maksimal 20 elemen.\n\n";
+	}
+	cout << "\n---------------------\n";
+	cout << "masukan elemen array\n";
+	cout << "------------------\n";
+	for (i = 0; i < n; i++) {
+		cout << "<" << (i + 1) << "> ";
+		cin >> arr[i];
 	}
 }
